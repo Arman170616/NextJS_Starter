1fdeb2c786d500e788c7f27316ce4c538afd6025
Arman170616 <armanice09@gmail.com>
2022-12-29 18:02:15 +0600

    Head

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,12 @@
+ import Head from 'next/head'
 const about = () => {
 
     return (
         <div>
-            <h1>About</h1>
+            <Head>
+                <title> About me </title>
+
+            </Head>
         </div>
     )
 }
